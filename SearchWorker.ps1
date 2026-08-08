5de11997c1fa24108ebcf13f9c4c9c35827d3c0a
fizz <fizz.from.outerspace@gmail.com>
2026-08-09 01:08:09 +0900

    test pre-commit

--- SearchWorker.ps1
+++ SearchWorker.ps1
@@ -1,5 +1,4 @@
 ﻿param($folderPath, $keyword, $fixGarbled, $ff14Mode, $scriptDir)
-$OutputEncoding = [System.Text.Encoding]::UTF8
 
 if ([string]::IsNullOrEmpty($scriptDir)) {
   $scriptDir = $PSScriptRoot
